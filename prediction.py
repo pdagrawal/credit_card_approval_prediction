93ab40aed24a14a0c4ff9509ff62633f5218f0ea
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2022-05-02 13:07:07 -0400

    added code

--- prediction.py
+++ prediction.py
@@ -28,3 +28,145 @@ plt.rcParams['figure.facecolor'] = 'white'
 begin_month=pd.DataFrame(record.groupby(["ID"])["MONTHS_BALANCE"].agg(min))
 begin_month=begin_month.rename(columns={'MONTHS_BALANCE':'begin_month'})
 new_data=pd.merge(data,begin_month,how="left",on="ID")
+
+# Generally, users in risk should be in 3%, thus I choose
+# users who overdue for more than 60 days as target risk users.
+# Those samples are marked as '1', else are '0'.
+record['dep_value'] = None
+record['dep_value'][record['STATUS'] =='2']='Yes'
+record['dep_value'][record['STATUS'] =='3']='Yes'
+record['dep_value'][record['STATUS'] =='4']='Yes'
+record['dep_value'][record['STATUS'] =='5']='Yes'
+
+cpunt = record.groupby('ID').count()
+cpunt['dep_value'][cpunt['dep_value'] > 0] = 'Yes'
+cpunt['dep_value'][cpunt['dep_value'] == 0] = 'No'
+cpunt = cpunt[['dep_value']]
+new_data = pd.merge(new_data, cpunt, how='inner', on='ID')
+new_data['target'] = new_data['dep_value']
+new_data.loc[new_data['target'] == 'Yes', 'target'] = 1
+new_data.loc[new_data['target'] == 'No', 'target'] = 0
+
+print(cpunt['dep_value'].value_counts())
+cpunt['dep_value'].value_counts(normalize=True)
+
+
+new_data.rename(columns={'CODE_GENDER':'Gender','FLAG_OWN_CAR':'Car','FLAG_OWN_REALTY':'Reality',
+                         'CNT_CHILDREN':'ChldNo','AMT_INCOME_TOTAL':'inc',
+                         'NAME_EDUCATION_TYPE':'edutp','NAME_FAMILY_STATUS':'famtp',
+                        'NAME_HOUSING_TYPE':'houtp','FLAG_EMAIL':'email',
+                         'NAME_INCOME_TYPE':'inctp','FLAG_WORK_PHONE':'wkphone',
+                         'FLAG_PHONE':'phone','CNT_FAM_MEMBERS':'famsize',
+                        'OCCUPATION_TYPE':'occyp'
+                        },inplace=True)
+
+new_data.dropna()
+new_data = new_data.mask(new_data == 'NULL').dropna()
+print('-------------')
+print(new_data)
+print('-------------')
+
+ivtable = pd.DataFrame(new_data.columns, columns=['variable'])
+ivtable['IV'] = None
+namelist = ['FLAG_MOBIL', 'begin_month', 'dep_value', 'target', 'ID']
+
+for i in namelist:
+    ivtable.drop(ivtable[ivtable['variable'] == i].index, inplace=True)
+
+
+# Calculate information value
+def calc_iv(df, feature, target, pr=False):
+    lst = []
+    df[feature] = df[feature].fillna("NULL")
+
+    for i in range(df[feature].nunique()):
+        val = list(df[feature].unique())[i]
+        lst.append([feature,                                                        # Variable
+                    val,                                                            # Value
+                    df[df[feature] == val].count()[feature],                        # All
+                    df[(df[feature] == val) & (df[target] == 0)].count()[feature],  # Good (think: Fraud == 0)
+                    df[(df[feature] == val) & (df[target] == 1)].count()[feature]]) # Bad (think: Fraud == 1)
+
+    data = pd.DataFrame(lst, columns=['Variable', 'Value', 'All', 'Good', 'Bad'])
+    data['Share'] = data['All'] / data['All'].sum()
+    data['Bad Rate'] = data['Bad'] / data['All']
+    data['Distribution Good'] = (data['All'] - data['Bad']) / (data['All'].sum() - data['Bad'].sum())
+    data['Distribution Bad'] = data['Bad'] / data['Bad'].sum()
+    data['WoE'] = np.log(data['Distribution Good'] / data['Distribution Bad'])
+
+    data = data.replace({'WoE': {np.inf: 0, -np.inf: 0}})
+
+    data['IV'] = data['WoE'] * (data['Distribution Good'] - data['Distribution Bad'])
+
+    data = data.sort_values(by=['Variable', 'Value'], ascending=[True, True])
+    data.index = range(len(data.index))
+
+    if pr:
+        print(data)
+        print('IV = ', data['IV'].sum())
+
+    iv = data['IV'].sum()
+    print('This variable\'s IV is:',iv)
+    print(df[feature].value_counts())
+    return iv, data
+
+def convert_dummy(df, feature,rank=0):
+    pos = pd.get_dummies(df[feature], prefix=feature)
+    mode = df[feature].value_counts().index[rank]
+    biggest = feature + '_' + str(mode)
+    pos.drop([biggest],axis=1,inplace=True)
+    df.drop([feature],axis=1,inplace=True)
+    df=df.join(pos)
+    return df
+
+def get_category(df, col, binsnum, labels, qcut = False):
+    if qcut:
+        localdf = pd.qcut(df[col], q = binsnum, labels = labels) # quantile cut
+    else:
+        localdf = pd.cut(df[col], bins = binsnum, labels = labels) # equal-length cut
+
+    localdf = pd.DataFrame(localdf)
+    name = 'gp' + '_' + col
+    localdf[name] = localdf[col]
+    df = df.join(localdf[name])
+    df[name] = df[name].astype(object)
+    return df
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+new_data['Gender'] = new_data['Gender'].replace(['F','M'],[0,1])
+print('-----------')
+print(new_data)
+print('-----------')
+print(new_data['Gender'].value_counts())
+iv, data = calc_iv(new_data,'Gender','target')
+ivtable.loc[ivtable['variable']=='Gender','IV']=iv
+data.head()
+
+
+
