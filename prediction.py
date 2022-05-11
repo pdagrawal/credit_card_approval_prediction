7272a1e893cd925d7a5b602e99b337d9668868c9
srbvik92 <srbvik92@gmail.com>
2022-05-11 18:56:36 -0400

    updated

--- prediction.py
+++ prediction.py
@@ -1,10 +1,5 @@
 import warnings
 warnings.filterwarnings('ignore')
-import pandas as pd
-import matplotlib.pyplot as plt
-<<<<<<< HEAD
-import warnings
-warnings.filterwarnings('ignore')
 import numpy as np
 import pandas as pd   
 import matplotlib.pyplot as plt
@@ -22,24 +17,6 @@ from sklearn.tree import DecisionTreeClassifier
 #from catboost import CatBoostClassifier
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
-=======
-import numpy as np
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# from imblearn.over_sampling import SMOTE
-# import itertools
-
-# from sklearn.model_selection import train_test_split
-# from sklearn.metrics import accuracy_score, confusion_matrix
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.tree import DecisionTreeClassifier
-
-# from xgboost import XGBClassifier
-# from lightgbm import LGBMClassifier
-# from catboost import CatBoostClassifier
-# from sklearn import svm
-# from sklearn.ensemble import RandomForestClassifier
->>>>>>> 66f0ab51ad5b132741fd70fc72febcf36e6772de
 
 data = pd.read_csv("input/application_record.csv", encoding = 'utf-8')
 record = pd.read_csv("input/credit_record.csv", encoding = 'utf-8')
